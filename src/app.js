f6a6bba84a2eec2539efc1734f71530dc604446a
Chugeno <eugenioazurmendi@gmail.com>
2024-05-22 17:21:43 -0300

    Hola

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a Mi Primera App</h1>
+        <p>Esta es una aplicación React de ejemplo creada en StackBlitz.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
